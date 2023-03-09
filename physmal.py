43528ab276b3867631dfa3f6a819a61d4c404787
Alexander Burgos <alexander.burgos@unmsm.edu.pe>
2023-03-09 13:35:33 -0500

    Mostrar funciones disponibles.

--- physmal.py
+++ physmal.py
@@ -1,26 +1,39 @@
-## Comentario
+## Script de interacción con el usuario para el uso del programa physmal.
 
 
+# Temas disponibles.
+temas = [
+    "Tranformaciones de Lorentz",
+    "Transformaciones inversas de lorentz",
+    "Dilatación del tiempo",
+    "Contracción de la longitud",
+    "Pitágoras",
+    "Dispersión de Compton",
+    "Líneas espectrales"
+    ]
 
-funciones_rel_esp = ["dilatación", "contracción"]
-
+temas.sort() # Ordena los temas alfabéticamente
 
 # Interacción con el usuario.
 
-print("Bienvenido a Physmal.")
-respuesta_1 = input("""
-¿Qué es lo que quieres resolver?
+print("Bienvenido a Physmal.\nEscriba el número correspondiente:")
+
+respuesta_0 = input("""
+1. Escribir problema.
+2. Mostrar los problemas disponibles.
 """)
 
+if respuesta_0 == "1":
+    respuesta_1 = input("""
+    ¿Qué es lo que quieres resolver?
+    """)
 
-if respuesta_1 == "pitagoras":
-    from Funciones.Generales.pitágoras import *
-    print(pitagoras(*input_pit()))
+    if respuesta_1 == "pitagoras":
+        from Funciones.Generales.pitágoras import *
+        print(pitagoras(*input_pit()))
 
-    
-
-
-# respuesta_2 = int(input("""
-# 1. dilataciónpigh
-# 2. contracción
-# """))
+elif respuesta_0 == "2":
+    print("-----------------------------")
+    for tema in temas:
+        print(tema)
+    print("-----------------------------")
