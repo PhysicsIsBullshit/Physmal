047008a6d813abf102a54c41fa9422eb41157f42
Alexander Burgos <alexander.burgos@unmsm.edu.pe>
2023-03-14 23:47:16 -0500

    Problemas dilatación y contracción relativistas añadidos a physmal.py.

--- physmal.py
+++ physmal.py
@@ -10,8 +10,23 @@ if respuesta_0 == 1:
     from Funciones.Generales.pitágoras import *
     print(pitagoras(*input_pit()))
 
+
 # Transformación galileana posición
 
 if respuesta_0 == 2:
     from Funciones.transformaciones_galileanas import *
     t_galileana()
+
+
+# Dilatación del tiempo
+
+if respuesta_0 == 6:
+    from Funciones.FísicaModerna.dilatación_tiempo import input_dilatación_tiempo
+    input_dilatación_tiempo()
+    
+
+# Contracción de la longitud
+
+if respuesta_0 == 7:
+    from Funciones.FísicaModerna.contracción_longitud import input_contracción_longitud
+    input_contracción_longitud()
